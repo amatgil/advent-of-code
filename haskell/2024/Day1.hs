912d78dd790684b9b4908b1c15f6a39ec643ea04
amatgil <amatgilvinyes@gmail.com>
2025-02-04 21:07:28 +0100

    Add haskell 2024/D1

--- haskell/2024/Day1.hs
+++ haskell/2024/Day1.hs
@@ -1,12 +1,24 @@
-{-# LANGUAGE MultilineStrings#-} 
 module Day1 where
+import Data.List (transpose, sort)
+
 
 sample =
-"""
-3   4
-4   3
-2   5
-1   3
-3   9
-3   3
-"""
+  "3   4\n\
+  \4   3\n\
+  \2   5\n\
+  \1   3\n\
+  \3   9\n\
+  \3   3\n"
+
+parse :: String -> ([Int], [Int])
+parse input = (left, right) where
+  dat = transpose $ fmap (fmap read) $ map words $ lines input
+  left = head dat
+  right = last dat
+
+part1 :: ([Int], [Int]) -> Int
+part1 (left, right) = sum $ zipWith (-) (sort right) (sort left)
+
+part2 :: ([Int], [Int]) -> Int
+part2 (left, right) = sum $ map (\n -> n * countInstances n right) left
+  where countInstances x = length . filter (== x) 
