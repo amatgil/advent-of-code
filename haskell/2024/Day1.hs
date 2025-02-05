4602db6db634c2d2bbd689419847266c7cc7de84
amatgil <amatgilvinyes@gmail.com>
2025-02-05 08:09:51 +0100

    Solve 2024/D1 in haskell

--- haskell/2024/Day1.hs
+++ haskell/2024/Day1.hs
@@ -1,6 +1,7 @@
-module Day1 where
-import Data.List (transpose, sort)
+module Day1 (day1) where
 
+import Data.List (sort, transpose)
+import Text.Printf
 
 sample =
   "3   4\n\
@@ -11,14 +12,20 @@ sample =
   \3   3\n"
 
 parse :: String -> ([Int], [Int])
-parse input = (left, right) where
-  dat = transpose $ fmap (fmap read) $ map words $ lines input
-  left = head dat
-  right = last dat
+parse input = (left, right)
+  where
+    [left, right] = transpose $ fmap (fmap read) $ map words $ lines input
 
 part1 :: ([Int], [Int]) -> Int
-part1 (left, right) = sum $ zipWith (-) (sort right) (sort left)
+part1 (left, right) = sum $ map abs $ zipWith (-) (sort right) (sort left)
 
 part2 :: ([Int], [Int]) -> Int
 part2 (left, right) = sum $ map (\n -> n * countInstances n right) left
-  where countInstances x = length . filter (== x) 
+  where
+    countInstances x = length . filter (== x)
+
+day1 :: IO ()
+day1 = do
+  input <- readFile "inputs/day1.txt"
+  printf "Day 1, Part 1: %d\n" $ part1 $ parse input
+  printf "Day 1, Part 2: %d\n" $ part2 $ parse input
