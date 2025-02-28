be9d7429105c1e6d181ee66ea02e43720aea74c9
amatgil <amatgilvinyes@gmail.com>
2025-02-28 22:43:53 +0100

    Generalize `curry`

--- elisp/2024/utils.el
+++ elisp/2024/utils.el
@@ -1,5 +1,8 @@
-(defmacro curry (f bound)
-  `(lambda (x) (,f ,bound x)))
+(defmacro curry (f &rest bound)
+  `(lambda (x) (,f ,@bound x)))
+
+(defmacro backcurry (bound f)
+  `(lambda (x) (,f ,@bound x)))
 
 (defun trim-trailing-newline (text)
   (string-trim-right text "\n"))
