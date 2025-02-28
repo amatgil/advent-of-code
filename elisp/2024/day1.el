56f8ccb063d1389d9a12efb183c736c4c235eb85
amatgil <amatgilvinyes@gmail.com>
2025-02-28 01:45:12 +0100

    Elisp D1P2

--- elisp/2024/day1.el
+++ elisp/2024/day1.el
@@ -1,3 +1,5 @@
+(load-file "utils.el")
+
 (setq sample "3   4
 4   3
 2   5
@@ -26,5 +28,17 @@
                       (sort (car parsed) '<)
                       (sort (cadr parsed) '<)))))
 
+(defun part2 (input)
+  (cl-reduce '+
+             (let* ((parsed (parse input))
+                    (left (car parsed))
+                    (right (cadr parsed)))
+               (mapcar* '*
+                        (mapcar (lambda (l)
+                                  (length (-filter (curry equal l) right)))
+                                left)
+                        left))))
 
 (format "Part 1: %d" (part1 (f-read "../inputs/2024-01.txt")))
+(format "Part 2: %d" (part2 (f-read "../inputs/2024-01.txt")))
+
