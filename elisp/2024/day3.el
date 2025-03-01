db43eae062c234829ada8c0f974d3127f1216ceb
amatgil <amatgilvinyes@gmail.com>
2025-03-01 14:34:52 +0100

    Elisp D3P2

--- elisp/2024/day3.el
+++ elisp/2024/day3.el
@@ -9,19 +9,18 @@
 (defun is-digit (d) (and (<= ?0 d) (>= ?9 d)))
 
 (defun digitlist-to-int (l)
-  "Expects non-empty list of digits. If it is empty, it will return 0. If it does not contain digits, output is undefined"
+  "Expects non-empty list of digits. If it is empty, it will return 0. If it
+does not contain digits, output is undefined"
   (if (null l) 0
     (+ (- (car (last l)) ?0)
        (* 10 (digitlist-to-int (butlast l))))))
 
-
 (defun parse-int (s)
-  "Expects l to be a list of chars, not a string. Returns either nil or a cons cell of the number and the non-consumed part of the string"
+  "Expects l to be a list of chars, not a string. Returns either nil or a
+cons cell of the number and the non-consumed part of the string"
   (let ((digits (-take-while (lambda (c) (is-digit c)) s)))
     (if (equal 0 (length digits)) (cons nil s)
       (cons (digitlist-to-int digits) (-drop (length digits) s)))))
-
-(-find-index (lambda (c) (not (is-digit c))) (string-to-list "123"))
 
 (defun dropprefix (l)
   "returns (t . rest) or (nil . input)"
@@ -63,15 +62,6 @@
                                              1)) ; close paren
       nil)))
 
-;; (defun extract-mul-pairs (acc s)
-;;   "Expects list of chars, not string"
-;;   (if (null s) acc
-;;     (let ((r (parse-mul s)))
-;;       (if r (extract-mul-pairs
-;;              (+ acc (apply #'* (-take 2 r)))
-;;              (-drop (caddr r) s))
-;;         (extract-mul-pairs acc (cdr s))))))
-
 (defun extract-mul-pairs (s)
   (let ((acc 0))
     (while (not (null s))
@@ -82,10 +72,30 @@
           (setq s (cdr s)))))
     acc))
 
+(defun toggleable-extract-mul-pairs (s)
+  (let ((acc 0)
+        (enabled 't))
+    (while (not (null s))
+      (let ((r (parse-mul s))
+            (do (parse-do s))
+            (dont (parse-dont s)))
+        (cond ((car dont)
+               (setq enabled nil)
+               (setq s (cdr dont)))
+              ((car do) 
+               (setq enabled 't)
+               (setq s (cdr do)))
+              ((and r enabled) 
+               (setq acc (+ acc (apply #'* (-take 2 r))))
+               (setq s (-drop (caddr r) s)))
+              ('t (setq s (cdr s))))))
+    acc))
+
 (defun part1 (input)
   (extract-mul-pairs (string-to-list input)))
 
-(part1 sample)
+(defun part2 (input)
+  (toggleable-extract-mul-pairs (string-to-list input)))
 
-;(setq max-lisp-eval-depth 1000000)
 (print (format "Part 1: %d" (part1 (f-read "../inputs/2024-03.txt"))))
+(print (format "Part 2: %d" (part2 (f-read "../inputs/2024-03.txt"))))
