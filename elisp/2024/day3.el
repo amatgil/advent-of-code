95ac0943e2081d32fb051b445289ed4b58241abe
amatgil <amatgilvinyes@gmail.com>
2025-03-01 14:02:55 +0100

    Switch from recursive to iterative because elisp can't do tail recursion (sob)

--- elisp/2024/day3.el
+++ elisp/2024/day3.el
@@ -1,3 +1,4 @@
+;;; -*- lexical-binding: t -*- 
 (load-file "utils.el")
 
 (setq sample "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
@@ -5,6 +6,7 @@
 
 (defun string-to-list (s) (append s nil))
 (defun chardigit-to-digit (c) (- c ?0))
+(defun is-digit (d) (and (<= ?0 d) (>= ?9 d)))
 
 (defun digitlist-to-int (l)
   "Expects non-empty list of digits. If it is empty, it will return 0. If it does not contain digits, output is undefined"
@@ -12,7 +14,6 @@
     (+ (- (car (last l)) ?0)
        (* 10 (digitlist-to-int (butlast l))))))
 
-(defun is-digit (d) (and (<= ?0 d) (>= ?9 d)))
 
 (defun parse-int (s)
   "Expects l to be a list of chars, not a string. Returns either nil or a cons cell of the number and the non-consumed part of the string"
@@ -22,30 +23,34 @@
 
 (-find-index (lambda (c) (not (is-digit c))) (string-to-list "123"))
 
-(defconst mulprefix (string-to-list "mul("))
-
 (defun dropprefix (l)
   "returns (t . rest) or (nil . input)"
   (if (equal mulprefix (-take 4 l))
       (cons t (-drop 4 l))
     (cons nil l)))
 
-(defun parse-comma (s)
-  (if (equal ?, (car s)) s (cons nil s)))
-
-(defun parse-close (s)
-  (if (equal ?\) (car s)) s (cons nil s)))
-
 (defun int-length (n)
   (if (equal n 0) 1 (1+ (floor (log10 n)))))
 
-(defun extract-mul (l)
+(defmacro parse-prefix (fn-name prefix)
+  `(defun ,fn-name (s)
+     (if (equal `,(string-to-list ,prefix) (-take ,(length prefix) s))
+         (cons (-take ,(length prefix) s) (-drop ,(length prefix) s))
+       (cons nil s))))
+
+(parse-prefix parse-mul-prefix "mul(")
+(parse-prefix parse-do "do(")
+(parse-prefix parse-dont "don't(")
+(parse-prefix parse-comma ",")
+(parse-prefix parse-close ")")
+
+(defun parse-mul (l)
   "If valid, returns (x y <chars consumed>). If invalid, returns nil"
-  (let* ((prefix-ret   (dropprefix  l))
-         (lhs-ret      (parse-int   (cdr prefix-ret)))
-         (comma-ret    (parse-comma (cdr lhs-ret)))
-         (rhs-ret      (parse-int   (cdr comma-ret)))
-         (close-paren  (parse-close (cdr rhs-ret))))
+  (let* ((prefix-ret   (parse-mul-prefix  l))
+         (lhs-ret      (parse-int         (cdr prefix-ret)))
+         (comma-ret    (parse-comma       (cdr lhs-ret)))
+         (rhs-ret      (parse-int         (cdr comma-ret)))
+         (close-paren  (parse-close       (cdr rhs-ret))))
     (if (and (car prefix-ret)
              (car lhs-ret)
              (car comma-ret)
@@ -58,17 +63,29 @@
                                              1)) ; close paren
       nil)))
 
+;; (defun extract-mul-pairs (acc s)
+;;   "Expects list of chars, not string"
+;;   (if (null s) acc
+;;     (let ((r (parse-mul s)))
+;;       (if r (extract-mul-pairs
+;;              (+ acc (apply #'* (-take 2 r)))
+;;              (-drop (caddr r) s))
+;;         (extract-mul-pairs acc (cdr s))))))
+
 (defun extract-mul-pairs (s)
-  "Expects list of chars, not string"
-  (if (null s) nil
-    (let ((r (extract-mul s)))
-      (if r (cons (-take 2 r) (multiplicationing (-drop (caddr r) s)))
-        (multiplicationing (cdr s))))))
+  (let ((acc 0))
+    (while (not (null s))
+      (let ((r (parse-mul s)))
+        (if r (progn
+                (setq acc (+ acc (apply #'* (-take 2 r))))
+                (setq s (-drop (caddr r) s)))
+          (setq s (cdr s)))))
+    acc))
 
 (defun part1 (input)
-  (-reduce-from (lambda (acc pair) (+ acc (apply #'* pair)))
-                0
-                (extract-mul-pairs (string-to-list input))))
+  (extract-mul-pairs (string-to-list input)))
 
 (part1 sample)
 
+;(setq max-lisp-eval-depth 1000000)
+(print (format "Part 1: %d" (part1 (f-read "../inputs/2024-03.txt"))))
